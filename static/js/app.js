0bed5681c337f8ab4ad33c50b4e1f5e7f69ea368
Chia-hong Chou <chia-hongchou@Oliver-2.local>
2021-09-18 13:28:03 +0800

    product details page

--- static/js/app.js
+++ static/js/app.js
@@ -1,12 +1,26 @@
-const menuItems = document.getElementById('menu-items');
-menuItems.style.maxHeight = '0px';
+/**
+ * Menu Item RWD
+ */
+const menuItems = document.getElementById("menu-items");
+menuItems.style.maxHeight = "0px";
 
-const menuIcon = document.querySelector('.menu-icon');
-menuIcon.addEventListener('click', () => {
-  console.log('menu toggled!');
-  if (menuItems.style.maxHeight == '0px') {
-    menuItems.style.maxHeight = '200px';
-  } else if (menuItems.style.maxHeight === '200px') {
-    menuItems.style.maxHeight = '0px';
+const menuIcon = document.querySelector(".menu-icon");
+menuIcon.addEventListener("click", () => {
+  console.log("menu toggled!");
+  if (menuItems.style.maxHeight == "0px") {
+    menuItems.style.maxHeight = "200px";
+  } else if (menuItems.style.maxHeight === "200px") {
+    menuItems.style.maxHeight = "0px";
   }
+});
+
+/**
+ * Product Gallery
+ */
+const productImage = document.getElementById("product-img");
+const smallImages = document.querySelectorAll(".small-img");
+smallImages.forEach((i) => {
+  i.addEventListener("click", () => {
+    productImage.src = i.src;
+  });
 });
