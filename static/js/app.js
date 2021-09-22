1ee3f3bf858c46f406c1adddbb1e36f0d35e8884
Chia-hong Chou <chia-hongchou@Oliver-2.local>
2021-09-22 09:32:27 +0800

    account page, form animation

--- static/js/app.js
+++ static/js/app.js
@@ -24,3 +24,25 @@ smallImages.forEach((i) => {
     productImage.src = i.src;
   });
 });
+
+/**
+ * Account Form animation
+ */
+
+const loginForm = document.getElementById("login-form");
+const regForm = document.getElementById("reg-form");
+const indicator = document.getElementById("indicator");
+const loginTabBtn = document.getElementById("login-tab-btn");
+const regTabBtn = document.getElementById("reg-tab-btn");
+
+loginTabBtn.addEventListener("click", () => {
+  loginForm.style.transform = "translateX(300px)";
+  regForm.style.transform = "translateX(300px)";
+  indicator.style.transform = "translateX(0)";
+});
+
+regTabBtn.addEventListener("click", () => {
+  loginForm.style.transform = "translateX(0)";
+  regForm.style.transform = "translateX(0)";
+  indicator.style.transform = "translateX(100px)";
+});
